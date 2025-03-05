b051ef2ac29d23d57ff6ee348049c5df0c2fb415
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-03-05 16:02:46 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,4 @@
-
 // app/page.js
 export default function HomePage() {
-  return <div className="p-6 text-center">
-      <h1 className="text-2xl font-bold">Welcome to Monad Swap DApp</h1>
-      <p className="text-gray-600">DApp đang chạy trên Monad Testnet.</p>
-    </div>;
+  return <h2>Welcome to Next.js</h2>;
 }
