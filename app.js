e8589dff5992b9cd891bab4bf937a73b6aefbd11
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-03-05 15:58:43 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-6 text-center">
-      <h1 className="text-2xl font-bold">Welcome to Monad Swap DApp</h1>
-      <p className="text-gray-600">DApp đang chạy trên Monad Testnet.</p>
-    </main>
-  );
-}
